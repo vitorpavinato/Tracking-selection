2bccb7f2f660cc0a2e8e5b698dcb91f60484ba22
Vitor Pavinato <vitor.pavi@gmail.com>
2018-08-06 15:42:56 +0200

    update pooling script

--- src/reftableFromBatches.R
+++ src/reftableFromBatches.R
@@ -1,32 +1,86 @@
 #!/usr/bin/env Rscript
 
+##########################################################################################################
+##                            Tracking Selection in Time-Series Data with                               ## 
+##                                      Forward-time Simulations                                        ##
+##                                      SCRIPT TO POOL REFTABLE                                         ##
+##########################################################################################################
+
+## Vitor Pavinato
+## vitor.pavinato@supagro.fr
+## INRA
+
+### Remove last features
 rm(list=ls())
 ls()
+
+###########################################
+##              R PACKAGES               ##
+###########################################
+
+#install.packages(c("foreach", "doParallel", "parallel"),dependencies = T)
+
+###########################################
+##           GLOBAL SETTINGS             ##
+###########################################
 
 super_batch_number     <- 1
 number_of_batches      <- 1000 
 batch_size             <- 20
-working_dir            <- "home/pavinato/project/Tracking-selection/"
+working_dir            <- "home/pavinato/Desktop/Tracking-selection-02.1/"
 reftable_file_folder   <- "batch"
 reftable_file          <- "reference_table"
 pooled_reftable_output <- "results/" 
-pooled_global_reftable <- "pooled_global_reftable"
-pooled_locus_reftable  <- "pooled_locus_reftable"
+pooled_reftable_file   <- "pooled_reftable"
+parallel_pool          <- TRUE
+num_of_threads         <- 20
 
-pooled_raw_global_reftable <- NULL
-pooled_raw_locus_reftable <- NULL
-for (i in 1:number_of_batches){
-  if (file.exists(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))){
-    load(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))
+###########################################
+##         LOAD REQUIRED PACKAGES        ##
+###########################################
+
+# load required libraries
+if(parallel_pool){
+  library(foreach)    #
+  library(parallel)   # -> fo    r simulating in parallel
+  library(doParallel) #
+}
+
+
+###########################################
+##           POOLING REFTABLES           ##
+###########################################
+
+poolFromBatches <- function(i){
+    if (file.exists(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))){
+      load(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))
     
-    sim <- raw_reftable$sim + (i - 1) * batch_size
+      sim <- raw_reftable$sim + (i - 1) * batch_size
+      raw_reftable <- data.frame(sim, raw_reftable)
+      return(raw_reftable)
+    }
+}
+
+if(parallel_sims){
+  cl <- makeCluster(num_of_threads)
+  registerDoParallel(cl)
+  clusterEvalQ(cl)
+  
+  pooled_raw_reftable <- foreach(i=1:number_of_batches,.combine=rbind) %dopar% {
+                                                                                poolFromBatches(i=i)
+    }
+  stopCluster(cl)
+  
+} else {
+  pooled_raw_reftable <- NULL
+  for (i in 1:number_of_batches){
+    if (file.exists(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))){
+      load(paste0("/", working_dir, reftable_file_folder, ".", i, "/", reftable_file, ".RData"))
     
-    raw_global_reftable <- data.frame(sim, raw_reftable[, 2:94])
-    raw_global_reftable <- raw_global_reftable[!duplicated(raw_global_reftable$sim), ]
-    pooled_raw_global_reftable <- rbind(pooled_raw_global_reftable, raw_global_reftable)
-    
-    raw_locus_reftable <- data.frame(sim, raw_reftable[, c(3:11, 95:113, 12:94)])
-    pooled_raw_locus_reftable <- rbind(pooled_raw_locus_reftable, raw_locus_reftable)
+      sim <- raw_reftable$sim + (i - 1) * batch_size
+      raw_reftable <- data.frame(sim, raw_reftable)
+      pooled_raw_reftable <- rbind(pooled_raw_reftable, raw_reftable)
+    }
   }
 }
 
@@ -34,11 +88,8 @@ if (!file_test("-d", paste0("/", working_dir, pooled_reftable_output))){
   dir.create(file.path(paste0("/", working_dir, pooled_reftable_output)))
 }
 
-save(pooled_raw_global_reftable,
-     file=paste0("/", working_dir, pooled_reftable_output, pooled_global_reftable, "_", super_batch_number, ".RData"))
-
-save(pooled_raw_locus_reftable,
-     file=paste0("/", working_dir, pooled_reftable_output, pooled_locus_reftable, "_", super_batch_number, ".RData"))
+save(pooled_raw_reftable,
+     file=paste0("/", working_dir, pooled_reftable_output, pooled_reftable_file, "_", super_batch_number, ".RData"))
 
 gc()
 
